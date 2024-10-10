6244c8dd7f72965ccdb42f60db6a5151e30b2eb3
Tahazzot <iamtahazzot@gmail.com>
2024-10-10 13:49:10 +0600

    Update languages c++ basic variables

--- languages/cpp/basics/variables.cpp
+++ languages/cpp/basics/variables.cpp
@@ -2,8 +2,17 @@
 
 int main()
 {
-  // Declare a variable of type int
-  int favorite_number = 7;
-  std::cout << "My favorite number is " << favorite_number << std::endl;
-  return 0;
+    // Variables are memory locations that store values of a specific type
+    // Declare a variable of type int
+    int favorite_number = 7;
+    std::cout << "My favorite number is " << favorite_number << std::endl;
+
+    // Declare a variable of type double
+    double pi = 3.14159;
+    std::cout << "The value of pi is " << pi << std::endl;
+
+    // Declare a variable of type char
+    char letter = 'A';
+    std::cout << "The first letter of the alphabet is " << letter << std::endl;
+    return 0;
 }
